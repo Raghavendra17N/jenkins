c1d4c61fb60147a64c923c387f5b2ee468d00b2f
Raghavendra17N <raghava01995@gamil.com>
2021-08-19 11:11:05 +0530

    TFA3.1

--- Deploy.sh
+++ Deploy.sh
@@ -1,8 +1,10 @@
 source ~/.bash_profile
 cat /dev/null > ansible-playbook/inventory
+cd TF\ script/;
 terraform plan \
  -out out.plan 
 terraform apply out.plan
 sleep 10
+cd ../;
 sh ssh_connection.sh 
 ansible-playbook ansible-playbook/jenkins.yml -i ansible-playbook/inventory
